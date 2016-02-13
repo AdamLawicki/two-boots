74bb1e0c1d099235b3a247d843963bc082e2ec89
Adam Lawicki <AdamLawicki@users.noreply.github.com>
2016-02-13 10:22:46 -0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,16 @@
+</div> <!-- /.container -->
+
+    <footer class="blog-footer" id="navmenu">
+      <p>Blog template built for <a href="http://getbootstrap.com">Bootstrap</a> by <a href="https://twitter.com/itslawicki">@itslawicki</a>.</p>
+      <div id="navmenu" class="hvr-float"><p>
+        <a href="#"><i class="fa fa-arrow-up"></i>
+</a>
+      </p>
+    <?php wp_nav_menu( array( 'theme_location' => 'footer-menu' ) ); ?></div>
+    </footer>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<?php wp_footer(); ?>
+  </body>
+</html>
